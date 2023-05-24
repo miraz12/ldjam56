0f77dd8738ad1eabf1621c0460567c3be4b58aed
Anton Christoffersson <anton_christoffersson@hotmail.com>
2023-05-25 00:07:15 +0200

    Fixed unused stuff

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -20,12 +20,13 @@ Game::Game(GLFWwindow *window)
   Entity en = m_ECSManager->createEntity();
 
   GraphicsComponent *graphComp =
-      new GraphicsComponent(*new GltfObject("resources/Models/gltf/helmet/DamagedHelmet.gltf"));
-  // GraphicsComponent *graphComp = new GraphicsComponent(*new
-  // GltfObject("../glTF-Sample-Models/2.0/MetalRoughSpheres/glTF/MetalRoughSpheres.gltf"));
+  new GraphicsComponent(*new GltfObject("resources/Models/gltf/helmet/DamagedHelmet.gltf"));
+  // GraphicsComponent *graphComp = new GraphicsComponent(
+      // *new GltfObject("../glTF-Sample-Models/2.0/MetalRoughSpheres/glTF/MetalRoughSpheres.gltf"));
   PositionComponent *posComp = new PositionComponent();
   PhysicsComponent *phyComp = new PhysicsComponent(m_ECSManager);
-  m_ECSManager->addComponents<GraphicsComponent, PositionComponent>(en, graphComp, posComp);
+  m_ECSManager->addComponents<GraphicsComponent, PositionComponent, PhysicsComponent>(
+      en, graphComp, posComp, phyComp);
 
   // Entity en = m_ECSManager->createEntity();
   // GraphicsComponent *graphComp = new GraphicsComponent();
