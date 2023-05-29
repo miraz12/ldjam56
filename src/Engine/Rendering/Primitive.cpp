1ef1de7dd9e1dd3d668d0368973593af9a5ee7fe
Anton <anton_christoffersson@hotmail.com>
2023-05-29 10:27:37 +0200

    Missing files

---
+++ src/Engine/Rendering/Primitive.cpp
@@ -0,0 +1,12 @@
+#include "Primitive.hpp"
+
+void Primitive::draw() {
+
+  glBindVertexArray(m_vao);
+  if (m_drawType == 0) {
+    glDrawArrays(m_mode, 0, m_count);
+  } else {
+    glDrawElements(m_mode, m_count, m_type, (void *)(sizeof(char) * (m_offset)));
+  }
+  glBindVertexArray(0);
+}
---
+++ src/Engine/Rendering/Primitive.hpp
@@ -0,0 +1,36 @@
+#ifndef PRIMITIVE_H_
+#define PRIMITIVE_H_
+
+#include <map>
+class Primitive {
+public:
+  void draw();
+
+  struct AttribInfo {
+    uint32_t vbo;
+    size_t byteOffset;
+    size_t byteStride;
+    bool normalized;
+    int32_t componentType;
+    size_t count;
+    int32_t type;
+  };
+
+  int32_t m_material{-1};
+
+  uint32_t m_vbo;
+
+  uint32_t m_drawType{0}; // Either 0 = drawArrays or 1
+
+  uint32_t m_ebo;
+  uint32_t m_count;
+  uint32_t m_type;
+  uint32_t m_offset;
+
+  uint32_t m_vao;
+  uint32_t m_mode;
+  // std::vector<unsigned char> data;
+  std::map<std::string, AttribInfo> attributes;
+};
+
+#endif // PRIMITIVE_H_
