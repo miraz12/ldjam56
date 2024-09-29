5f835c3f3674d805544a34becf3581fd1d2cbeee
Anton Christoffersson <anton_christoffersson@hotmail.com>
2024-09-29 23:18:25 +0200

    Added default body for things without a mesh

--- src/Engine/ECS/Components/PhysicsComponent.cpp
+++ src/Engine/ECS/Components/PhysicsComponent.cpp
@@ -63,8 +63,38 @@ void PhysicsComponent::init() {
     btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState,
                                                     colShape, localInertia);
     body = new btRigidBody(rbInfo);
+
     body->setUserIndex(m_en);
 
+    auto &pSys = dynamic_cast<PhysicsSystem &>(eManager.getSystem("PHYSICS"));
+    pSys.addRigidBody(body);
+    initialized = true;
+  } else {
+    colShape =
+        new btBoxShape(btVector3(btScalar(1.), btScalar(1.), btScalar(1.)));
+    /// Create Dynamic Objects
+    startTransform.setIdentity();
+
+    // rigidbody is dynamic if and only if mass is non zero, otherwise static
+    bool isDynamic = (mass != 0.f);
+
+    btVector3 localInertia(0, 0, 0);
+    if (isDynamic)
+      colShape->calculateLocalInertia(mass, localInertia);
+
+    startTransform.setOrigin(initialPos);
+    startTransform.setRotation(initialRotation);
+
+    // using motionstate is recommended, it provides interpolation capabilities,
+    // and only synchronizes 'active' objects
+    myMotionState = new btDefaultMotionState(startTransform);
+    btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState,
+                                                    colShape, localInertia);
+    body = new btRigidBody(rbInfo);
+
+    body->setUserIndex(m_en);
+
+    ECSManager &eManager = ECSManager::getInstance();
     auto &pSys = dynamic_cast<PhysicsSystem &>(eManager.getSystem("PHYSICS"));
     pSys.addRigidBody(body);
     initialized = true;
