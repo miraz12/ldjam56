5ee4e2d3c394304cb39741c13de00a3fa0489005
Anton Christoffersson <anton_christoffersson@hotmail.com>
2024-09-30 20:17:29 +0200

    At compile move lib to resources/lib

--- src/Engine/engine_pch.hpp
+++ src/Engine/engine_pch.hpp
@@ -40,6 +40,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/euler_angles.hpp>
 
 // Bullet
 #include <BulletCollision/CollisionShapes/btBoxShape.h>
